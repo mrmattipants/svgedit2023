6ce6d0f7a84ce9b4363bad275e52e8c382dd5aa9
Matti Pedersen <mrmattipants@gmail.com>
2023-09-24 15:42:21 -0500

    Update en.js
    Added Raphael Icon Sets 3, 4 & 5

--- src/editor/extensions/ext-shapes/locale/en.js
+++ src/editor/extensions/ext-shapes/locale/en.js
@@ -14,7 +14,10 @@ export default {
     music: 'Music',
     misc: 'Miscellaneous',
     raphael_1: 'raphaeljs.com set 1',
-    raphael_2: 'raphaeljs.com set 2'
+    raphael_2: 'raphaeljs.com set 2',
+    raphael_3: 'raphaeljs.com set 3',
+    raphael_4: 'raphaeljs.com set 4',
+    raphael_5: 'raphaeljs.com set 5'
   },
   buttons: [
     {
